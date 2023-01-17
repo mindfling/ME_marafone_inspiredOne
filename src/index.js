129f038d2242210fd639d90268f2c3da9deba89b
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-01-17 15:06:40 +0300

    feat product btn-favorite click toggle

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './index.html';
 import './index.scss';
 
 console.log('Hi')
+
+// * обработка кнопки добавить в избранное
+const btnFavList = document.querySelectorAll('.product__btn-favorite');
+
+document.addEventListener('DOMContentLoaded', event => {
+
+  btnFavList.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+      console.log(e.target);
+      btn.classList.toggle('product__btn-favorite_active');
+    })
+  });
+
+});
