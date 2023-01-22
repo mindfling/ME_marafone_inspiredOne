494ec1646ecf48486f0742c0cb8851c3cfd9926e
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-01-22 19:27:02 +0300

    feat make gender navigation change

--- src/index.js
+++ src/index.js
@@ -13,9 +13,43 @@ import { renderHeader } from './modules/render/renderHeader';
 // * обработка кнопки добавить в избранное
 
 document.addEventListener('DOMContentLoaded', event => {
+  
   const btnFavList = document.querySelectorAll('.product__btn-favorite');
   const goods = document.querySelector('.goods');
 
+  const genderLinks = document.querySelectorAll('.gender__link');
+  const categoryLinks = document.querySelectorAll('.category__link');
+
+
+  
+  genderLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      genderLinks.forEach(elem => {
+        if (elem === e.target) {
+          elem.classList.add('gender__link_active');
+        } else {
+          elem.classList.remove('gender__link_active');
+        };
+      });
+    });
+  });
+
+
+  categoryLinks.forEach(link => {
+    link.addEventListener('click', event => {
+      const target = event.target;
+      categoryLinks.forEach(elem => {
+        if (elem === target) {
+          elem.classList.add('category__link_active');
+        } else {
+          elem.classList.remove('category__link_active');
+        }
+      })
+    })
+  })
+
+
+  // * click по цвету товара
   goods.addEventListener('click', (e) => {
     if (e.target.classList.contains('color')) {
       console.log('color')
@@ -23,6 +57,7 @@ document.addEventListener('DOMContentLoaded', event => {
     }
   });
 
+  // * click по сердечку
   btnFavList.forEach(btn => {
     btn.addEventListener('click', (e) => {
       console.log(e.target);
