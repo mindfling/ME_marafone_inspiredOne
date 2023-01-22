71bc6d38c71cf3c2f3fc0ff33be55a0f15bfbd2b
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-01-23 01:40:03 +0300

    feat category and gender menu nav

--- src/index.js
+++ src/index.js
@@ -16,19 +16,32 @@ document.addEventListener('DOMContentLoaded', event => {
   
   const btnFavList = document.querySelectorAll('.product__btn-favorite');
   const goods = document.querySelector('.goods');
-
+  
   const genderLinks = document.querySelectorAll('.gender__link');
   const categoryLinks = document.querySelectorAll('.category__link');
-
-
+  const hero = document.querySelector('.hero');
+  const heroTitle = hero.querySelector('.hero__title');
+  
+  const genders = ['women', 'men'];
+  const genderHeroTitles = {
+    'women': 'Новая коллекция зимней женской одежды',
+    'men': 'Новая коллекция зимней одежды для мужчин',
+  }
   
   genderLinks.forEach(link => {
     link.addEventListener('click', e => {
-      genderLinks.forEach(elem => {
+
+      genderLinks.forEach((elem, index) => {
         if (elem === e.target) {
           elem.classList.add('gender__link_active');
+          const heroClass = genders[index];
+          hero.classList.add('hero_' + heroClass);
+          heroTitle.textContent = genderHeroTitles[heroClass];
         } else {
+          const heroClass = genders[index];
+          // ? только для 2х элеменов
           elem.classList.remove('gender__link_active');
+          hero.classList.remove('hero_' + genders[index]);
         };
       });
     });
