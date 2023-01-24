fa26cf362c5bed1a3c724442c4ffeaea4ffa259d
Nepochtov Dmitry <78114857+mindfling@users.noreply.github.com>
2023-01-24 20:05:10 +0300

    add createElement

---
+++ src/modules/createElement.js
@@ -0,0 +1,38 @@
+export const createElement = (tag, attr, {append, appends, parent, callback, text} = {}) => {
+  if (typeof tag === 'string') {
+    if (tag === '') {
+      tag = 'div';
+    }
+  } else {
+    return;
+  }
+  const element = document.createElement(tag);
+  
+
+  if (attr) {
+    Object.assign(element, attr);
+  }
+
+  if (append && append instanceof HTMLElement) {
+    element.append(append);
+  }
+  
+  if (appends && appends.every(item => item instanceof HTMLElement)) {
+    element.append(...appends);
+  }
+  
+  if (parent && parent instanceof HTMLElement) {
+    parent.append(element);
+  }
+
+  if (callback && typeof callback === 'function') {
+    callback(element);
+  }
+
+  if (text && typeof text === 'string') {
+    console.log('STRING TEXT')
+    element.textContent = text;
+  }
+
+  return element;
+}
